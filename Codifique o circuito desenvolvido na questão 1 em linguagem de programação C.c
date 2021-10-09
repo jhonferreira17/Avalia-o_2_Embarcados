665ec29f7726e774c9fb82dd03ce72f85718225b
Jonata Ferreira Barbosa <48557997+jhonferreira17@users.noreply.github.com>
2021-10-09 11:14:09 -0300

    Add files via upload

---
+++ Codifique o circuito desenvolvido na questão 1 em linguagem de programação C.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a,b,x;
+	a = 0;
+	b = 0;
+	x = (!(a && b));
+	printf("OPERADOR AND\n");
+	printf("|%d|and|%d|invertido:%d\n",a,b,x);
+	a = 0;
+	b = 1;
+	x = (!(a && b));
+	printf("|%d|and|%d|invertido:%d\n",a,b,x);
+	a = 1;
+	b = 0;
+	x = (!(a && b));
+	printf("|%d|and|%d|invertido:%d\n",a,b,x);
+	a = 1;
+	b = 1;
+	x = (!(a && b));
+	printf("|%d|and|%d|invertido:%d\n",a,b,x);
+	return 0;
+}
