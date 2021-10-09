d5462774a703296743825d40c1d09f0bef32c433
Jonata Ferreira Barbosa <48557997+jhonferreira17@users.noreply.github.com>
2021-10-09 11:17:11 -0300

    Update and rename Codifique o circuito desenvolvido na questão 1 em linguagem de programação C.c to questão_1_em_linguagem_C.c

--- questão_1_em_linguagem_C.c
+++ questão_1_em_linguagem_C.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(){
 	int a,b,x;
-	a = 0;
-	b = 0;
-	x = (!(a && b));
+	  a = 0;
+	  b = 0;
+	  x = (!(a && b));
 	printf("OPERADOR AND\n");
 	printf("|%d|and|%d|invertido:%d\n",a,b,x);
-	a = 0;
-	b = 1;
-	x = (!(a && b));
+	  a = 0;
+	  b = 1;
+	  x = (!(a && b));
 	printf("|%d|and|%d|invertido:%d\n",a,b,x);
-	a = 1;
-	b = 0;
-	x = (!(a && b));
+	  a = 1;
+	  b = 0;
+	  x = (!(a && b));
 	printf("|%d|and|%d|invertido:%d\n",a,b,x);
-	a = 1;
-	b = 1;
-	x = (!(a && b));
+	  a = 1;
+	  b = 1;
+	  x = (!(a && b));
 	printf("|%d|and|%d|invertido:%d\n",a,b,x);
 	return 0;
 }
